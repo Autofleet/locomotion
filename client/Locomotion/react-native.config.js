336df8947e76662ef3bf00970ef0339f83a06f9a
Guy Serfaty <guy353@gmail.com>
2019-09-04 19:40:59 +0300

    Android

--- client/Locomotion/react-native.config.js
+++ client/Locomotion/react-native.config.js
@@ -12,6 +12,24 @@ module.exports = {
         // ios: null,
       },
     },
+    'react-native-device-info': {
+      platforms: {
+        android: null,
+        // ios: null,
+      },
+    },
+    'react-native-linear-gradient': {
+      platforms: {
+        android: null,
+        // ios: null,
+      },
+    },
+    'react-native-image-resizer': {
+      platforms: {
+        android: null,
+        // ios: null,
+      },
+    },
     'react-native-config': {
       platforms: {
         android: null,
