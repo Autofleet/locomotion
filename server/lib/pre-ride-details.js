6fbf9fcc07dc880703fc7c7cb02acd74a3c92672
Guy Serfaty <guy353@gmail.com>
2019-09-21 14:06:35 +0300

    Add pre ride details entry point + bring back places tests

---
+++ server/lib/pre-ride-details.js
@@ -0,0 +1,41 @@
+const axios = require('axios');
+
+const demandApi = axios.create({
+  baseURL: process.env.AF_BACKEND_URL,
+  headers: { Authorization: process.env.AF_API_TOKEN },
+});
+
+
+const getAfNearbyVehiclesWithEta = location => demandApi.get('api/v1/nearby-vehicles', {
+  params: {
+    location,
+    radius: process.env.RADIUS_FOR_CALC_PRE_RIDE_ETA,
+    preEta: 3,
+  },
+});
+
+const directions = async (origin, destination) => {
+  const { data } = await axios.get('https://maps.googleapis.com/maps/api/directions/json', {
+    params: {
+      key: process.env.GOOGLE_MAPS_API_KEY,
+      origin: `${origin.lat},${origin.lng}`,
+      destination: `${destination.lat},${destination.lng}`,
+    },
+  });
+  return data;
+};
+
+const getRouteDistance = async (origin, destination) => {
+  const directionResult = await directions(origin, destination);
+  return directionResult.routes[0].legs.reduce((routeDistance, leg) => routeDistance + leg.distance.value, 0);
+};
+
+module.exports = async (origin, destination) => {
+  const inAreaVehicles = await getAfNearbyVehiclesWithEta(origin);
+  const eta = (inAreaVehicles.slice(0, 3).reduce((count, vehicle) => count + vehicle.preEta, 0) / 3);
+  const routeDistance = await getRouteDistance(origin, destination);
+  const estimatePrice = 1.5 + ((routeDistance / 1000) * 0.5);
+
+  return { eta, estimatePrice };
+};
+
