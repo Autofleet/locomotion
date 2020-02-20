49d11b6ca36c76552ca27f094f49774454e0ca71
Ori Efrati <oritheman132@gmail.com>
2020-02-20 14:38:43 +0200

    cleanup

--- client/Locomotion/storybook/stories/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react-native';
-import Welcome from './Welcome';
-
-require('./RideDrawer');
-
-storiesOf('Welcome', module).add('to Storybook', () => <Welcome />);
