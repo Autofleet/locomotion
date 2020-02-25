9879c043f9814c9841f405412fb8e03d481a6ec2
Ori Efrati <oritheman132@gmail.com>
2020-02-25 23:13:35 +0200

    check if user active

--- locomotion-sdk/src/pages/Lock/index.js
+++ locomotion-sdk/src/pages/Lock/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Auth from '../../services/auth'
 import network from '../../services/network';
 import AppSettings from '../../services/app-settings';
@@ -11,8 +11,40 @@ import {
 import { LockTextContainer,ButtonContainer,SubText,HeaderText, Text } from './styled';
 import I18n from '../../I18n';
 import { useStateValue } from '../../context/main';
+import UserService from '../../services/user';
+
+const useInterval = (callback, delay) => {
+  const savedCallback = useRef();
+
+  // Remember the latest callback.
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  // Set up the interval.
+  useEffect(() => {
+    function tick() {
+      savedCallback.current();
+    }
+    if (delay !== null) {
+      const id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
+}
 
 export default ({ navigation }) => {
+  useInterval(async () => {
+    const userData = await UserService.getUser();
+    if(userData === null) {
+      Auth.logout(navigation)
+    }
+
+    if(userData.active === true) {
+      navigation.navigate('App');
+    }
+  }, 5000)
+
   const submit = async () => {
     Auth.logout(navigation)
   };
