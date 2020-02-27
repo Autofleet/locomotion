bc314c8bd51e7512e1d65a47b8cd68af3741c559
Ofry Linkovsky <15746116+OfryL@users.noreply.github.com>
2020-02-27 12:09:53 +0200

    fixes

--- locomotion-sdk/src/Components/Drawer/styled.js
+++ locomotion-sdk/src/Components/Drawer/styled.js
@@ -2,35 +2,34 @@ import styled from 'styled-components';
 const ArrowIconSource = require('../../assets/arrowright.png');
 
 const defaultTextColor = '#686868';
-const drawerPadding = 25;
+const drawerPadding = 26;
 
 export const DrawerIcon = styled.Image`
-  width: 20px;
-  height: 20px;
+  width: 23px;
+  height: 23px;
   margin-right: 20px;
   opacity: 0.6;
 `;
 
 export const Arrow = styled.Image.attrs({ source: ArrowIconSource })`
-  position: absolute;
-  right: 0;
-  top: ${(drawerPadding + 6)}px;
   opacity: 0.4;
   margin-right: 10px;
+  margin-left: auto;
   width: 10px;
   height: 10px;
+  margin-top: 10px;
 `;
 
 export const LabelText = styled.Text(props => `
   color: ${props.color || defaultTextColor};
-  margin-top: 1px;
+  margin-top: 4px;
 `);
 
 export const StyledDrawerLabel = styled.View`
-  margin: 0 35px;
+  margin: 0 20px;
   border-color: #dfdfdf;
   border-bottom-width: 1px;
-  padding: ${drawerPadding}px;
+  padding: ${drawerPadding}px 10px;
   width: 100%;
   flex-direction: row;
   flex: 1;
