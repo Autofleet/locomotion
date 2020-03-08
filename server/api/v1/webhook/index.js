f179307c3eacc0d3a75bb08771f35dbd8cab8812
Ori Efrati <oritheman132@gmail.com>
2020-03-08 23:39:13 +0200

    save notification and sending

--- server/api/v1/webhook/index.js
+++ server/api/v1/webhook/index.js
@@ -1,7 +1,9 @@
+const moment = require('moment');
 const Router = require('../../../lib/router');
 const rideService = require('../../../lib/ride');
-const { Ride, User } = require('../../../models');
+const { Ride, User, Notification } = require('../../../models');
 const sendNotification = require('../../../lib/push-notifications');
+const settingsService = require('../../../lib/settings');
 
 const router = Router();
 
@@ -11,6 +13,45 @@ router.put('/:rideId', async (req, res) => {
       id: req.params.rideId,
     },
   });
+
+  const stopPoints = req.body.ride.stop_points;
+  const { value: arriveMin } = await settingsService.getSettingByKeyFromDb('ARRIVE_REMINDER_MIN');
+
+  const etaTime = moment(stopPoints[0].eta);
+  const diff = etaTime.diff(moment(), 'minutes');
+
+  if (stopPoints[0].completed_at === null && diff <= arriveMin) {
+    const user = await User.findOne({
+      where: {
+        id: ride.userId,
+      },
+    });
+
+    if (user) {
+      const prevNotification = await Notification.findOne({
+        where: {
+          userId: user.id,
+          rideId: ride.id,
+          type: 'driverArriving',
+        },
+      });
+
+      if (!prevNotification) {
+        await Notification.create({
+          userId: user.id,
+          rideId: ride.id,
+          type: 'driverArriving',
+          content: {
+            targetIdsRaw: [user.pushUserId],
+            notificationId: 'driverArriving',
+            contents: { en: `Driver arriving in ${arriveMin} minutes to ${stopPoints[0].description}` },
+            headings: { en: 'Driver is arriving!' },
+            ttl: { ttl: 60 * 30 },
+          },
+        });
+      }
+    }
+  }
 
   if (!ride) {
     res.json({ error: 'ride not found' });
@@ -41,36 +82,34 @@ router.put('/:rideId', async (req, res) => {
   res.json(ride);
 });
 
-router.get('/:rideId/notification', async (req, res) => {
-  const ride = await Ride.findOne({
+router.get('/notifications', async (req, res) => {
+  const notifications = await Notification.findAll({
     where: {
-      id: req.params.rideId,
+      state: Notification.STATES.PENDING,
     },
   });
 
-  if (!ride) {
-    res.json({ error: 'ride not found' });
-  }
+  notifications.map(async (notificationRecord) => {
+    const notificationContent = notificationRecord.content;
+    try {
+      await sendNotification(
+        notificationContent.targetIdsRaw,
+        notificationContent.type,
+        notificationContent.contents,
+        notificationContent.headings,
+        notificationContent.ttl,
+      );
 
-  const user = await User.findOne({
-    where: {
-      id: ride.userId,
-    },
+      notificationRecord.state = Notification.STATES.COMPLETED;
+      notificationRecord.save();
+    } catch (err) {
+      console.log('Push Error', err);
+      notificationRecord.state = Notification.STATES.REJECTED;
+      notificationRecord.save();
+    }
   });
 
-  if (!user) {
-    res.json({ error: 'user not found' });
-  }
-
-  const pushNotification = await sendNotification(
-    [user.pushUserId],
-    'driverArriving',
-    { en: ' to stop point X' },
-    { en: 'Driver arriving in 3 minutes' },
-    { ttl: 60 * 30 },
-  );
-
-  res.json(pushNotification.data);
+  res.json(200);
 });
 
 module.exports = router;
