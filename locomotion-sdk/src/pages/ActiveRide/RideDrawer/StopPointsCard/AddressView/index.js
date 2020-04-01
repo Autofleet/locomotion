4d5eb2c8992a1a57c1f3c38c4b878ba9b072fa2a
Ori Efrati <oritheman132@gmail.com>
2020-04-01 16:16:44 +0300

    handle list by location

--- locomotion-sdk/src/pages/ActiveRide/RideDrawer/StopPointsCard/AddressView/index.js
+++ locomotion-sdk/src/pages/ActiveRide/RideDrawer/StopPointsCard/AddressView/index.js
@@ -1,12 +1,9 @@
-import React, { useState, useRef, Fragment } from 'react';
-import {
-  ScrollView,
-  View,
-} from 'react-native';
-import getPosition from './getPostion';
-import network from '../../../../../services/network';
+import React, { useState, useEffect, useRef, Fragment } from "react";
+import { ScrollView, View } from "react-native";
+import getPosition from "./getPostion";
+import network from "../../../../../services/network";
 
-import I18n from '../../../../../I18n';
+import I18n from "../../../../../I18n";
 import {
   AddressInputs,
   AddressInputsHeader,
@@ -24,83 +21,97 @@ import {
   HeaderIcon,
   ResetInputIconContainer,
   ResetInputIcon
-} from './styled';
-import PageHeader from '../../../../../Components/PageHeader';
-import SafeView from '../../../../../Components/SafeView';
+} from "./styled";
+import PageHeader from "../../../../../Components/PageHeader";
+import SafeView from "../../../../../Components/SafeView";
 
-const closeIconSource = require('../../../../../assets/arrow-back.png');
+const closeIconSource = require("../../../../../assets/arrow-back.png");
 
-export default (props) => {
-
+export default props => {
   const [searchText, setSearchText] = useState(
-    props.requestStopPoints[props.type]
-    && props.requestStopPoints[props.type].description,
+    props.requestStopPoints[props.type] &&
+      props.requestStopPoints[props.type].description
   );
-
   const [addressListItems, setAddressListItems] = useState(null);
+  const [coords, setCoords] = useState(null)
 
-  const enrichPlaceWithLocation = async (place) => {
-    const { data } = await network.get('api/v1/me/places/get-location', {
+  useEffect(() => {
+    initCurrentLocation()
+  }, [])
+
+  const initCurrentLocation = async () => {
+    const { coords } = await getPosition();
+    setCoords(coords)
+  }
+
+  const enrichPlaceWithLocation = async place => {
+    const { data } = await network.get("api/v1/me/places/get-location", {
       params: {
-        placeId: (place.placeid || place.place_id),
-      },
+        placeId: place.placeid || place.place_id
+      }
     });
     place = { ...place, ...data };
     return place;
   };
 
-  const setPlace = async (place) => {
-    setAddressListItems({
-      type: addressListItems.type,
-      list: [],
-    });
-    setSearchValue(place.description, props.type, true);
-
+  const setPlace = async place => {
     if (!place.lat && (place.placeid || place.place_id)) {
       place = await enrichPlaceWithLocation(place);
     }
 
-    if(place.station) {
+    if (place.station) {
       if (props.onLocationSelect) {
         props.onLocationSelect({
           ...place,
-          type: addressListItems.type,
+          type: addressListItems.type
         });
       }
       setAddressListItems({
         type: addressListItems.type,
-        list: [],
+        list: []
       });
-
+    } else {
+      setAddressListItems({
+        type: addressListItems.type,
+        list: []
+      });
+      setSearchValue(place.description, props.type, true, place);
     }
-
-/*    */
   };
 
-  const loadAddress = async (input, showStations) => {
+  const loadAddress = async (input, showStations, selectedPlace = null) => {
+    let location = null;
     try {
-      const { coords } = await getPosition();
-      const { data } = await network.get('api/v1/me/places', {
+      if (selectedPlace) {
+        location = {
+          lat: selectedPlace.lat,
+          lng: selectedPlace.lng
+        };
+      } else {
+        location = { lat: coords.latitude, lng: coords.longitude };
+      }
+
+      const { data } = await network.get("api/v1/me/places", {
         params: {
           input,
-          location: { lat: coords.latitude, lng: coords.longitude },
-          stations:showStations
-        },
+          location,
+          stations: showStations
+        }
       });
-
       return data;
     } catch (error) {
-      console.log('Got error while try to get places', error);
+      console.log("Got error while try to get places", error);
       return undefined;
     }
   };
 
-  const setSearchValue = async (value, type, showStations = null) => {
+  const setSearchValue = async (value, type, showStations = null, place) => {
     setSearchText(value);
-
     setAddressListItems({
       type,
-      ...{ list: value ? await loadAddress(value,showStations) : undefined },
+      ...{
+        list: value ? await loadAddress(value, showStations, place) : undefined
+      }
     });
   };
 
@@ -111,27 +122,23 @@ export default (props) => {
           value={searchText}
           onChangeText={value => setSearchValue(value, props.type)}
           autoFocus
-          placeholder={I18n.t('addressView.pickupPlaceholder')}
+          placeholder={I18n.t("addressView.pickupPlaceholder")}
         />
       </Address>
 
       <ScrollView>
-        {addressListItems && addressListItems.list && addressListItems.list.map(item => (
-          <AddressSearchItem
-            key={item.id}
-            onPress={() => setPlace(item)}
-          >
-            <AddressSearchItemText>
-              {item.description}
-            </AddressSearchItemText>
-            {item.distance
-              ? (
+        {addressListItems &&
+          addressListItems.list &&
+          addressListItems.list.map(item => (
+            <AddressSearchItem key={item.id} onPress={() => setPlace(item)}>
+              <AddressSearchItemText>{item.description}</AddressSearchItemText>
+              {item.distance ? (
                 <DistanceFromAddress>
                   {item.distance ? `${item.distanceFromMe.toFixed(2)}km` : null}
                 </DistanceFromAddress>
               ) : null}
-          </AddressSearchItem>
-        ))}
+            </AddressSearchItem>
+          ))}
       </ScrollView>
       <ResetInputIconContainer onPress={props.onClose}>
         <ResetInputIcon />
