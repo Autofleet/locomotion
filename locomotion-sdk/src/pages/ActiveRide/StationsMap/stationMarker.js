187bd55dfb688f433d6b4a761b647c7d2feee672
Ori Efrati <oritheman132@gmail.com>
2020-04-10 16:49:27 +0300

    custom marker fix for ios and google maps

--- locomotion-sdk/src/pages/ActiveRide/StationsMap/stationMarker.js
+++ locomotion-sdk/src/pages/ActiveRide/StationsMap/stationMarker.js
@@ -1,12 +1,27 @@
 import React from 'react';
+import {Platform} from 'react-native'
 import { Callout, Marker } from 'react-native-maps';
-import CustomMarker from './CustomMarker'
+
+import AppleCustomMarker from './AppleCustomMarker'
+import GoogleCustomMarker from './GoogleCustomMarker'
+import Config from 'react-native-config';
+
+const CustomMarkerSelector = (props) => {
+  if(Platform.OS === 'ios') {
+    if(Config.MAP_PROVIDER !== 'google') {
+      return AppleCustomMarker;
+    }
+  }
+
+  return GoogleCustomMarker;
+}
 
 export default ({
   stationKey, lat, lng, selectStation, requestStopPoints, isInOffer,
 }) => {
   const pickup = requestStopPoints.pickup && requestStopPoints.pickup.lng === lng && requestStopPoints.pickup.lat === lat;
   const dropoff = requestStopPoints.dropoff && requestStopPoints.dropoff.lng === lng && requestStopPoints.dropoff.lat === lat;
+  const CustomMarker = CustomMarkerSelector();
 
   const getMarker = () => {
     if (pickup) {
@@ -29,7 +44,6 @@ export default ({
       coordinate={{ latitude: lat, longitude: lng }}
       onPress={e => !isInOffer && selectStation(stationKey, pickup, dropoff)}
       style={(pickup || dropoff) ? { zIndex: 1 } : {}}
-      tracksViewChanges={false}
       key={stationKey}
     >
       {getMarker()}
