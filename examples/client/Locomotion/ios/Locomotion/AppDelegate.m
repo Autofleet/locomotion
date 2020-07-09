e54069ecc7038bead4aa31f097e211e6cf500547
Ori Efrati <oritheman132@gmail.com>
2020-07-09 19:32:10 +0300

    bump versions

--- examples/client/Locomotion/ios/Locomotion/AppDelegate.m
+++ examples/client/Locomotion/ios/Locomotion/AppDelegate.m
@@ -17,7 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  [GMSServices provideAPIKey:@"_YOUR_API_KEY_"];
+  [GMSServices provideAPIKey:@"AIzaSyAIHdR9FzTpt8S_fxDTZVZsDqxe3RWk1AY"];
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
                                                    moduleName:@"Locomotion"
