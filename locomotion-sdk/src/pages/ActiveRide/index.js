3959f08fc5cf8eee19a2fad1d41a8e1fa3f885f3
Ofry Linkovsky <15746116+OfryL@users.noreply.github.com>
2020-08-05 12:08:46 +0300

    refactor

--- locomotion-sdk/src/pages/ActiveRide/index.js
+++ locomotion-sdk/src/pages/ActiveRide/index.js
@@ -344,26 +344,20 @@ export default ({ navigation, menuSide, mapSettings }) => {
   };
 
   const getStations = async () => {
-    let lat, lng;
-
     try {
       const { coords } = await getPosition();
       if(coords.latitude && coords.longitude) {
-        lat = coords.latitude
-        lng = coords.longitude
+        const { latitude: lat, longitude: lng} = coords;
+        const {data} = await network.get('api/v1/me/places', {
+          params: {
+            location: {lat, lng},
+            stations: true,
+          },
+        });
+        setStations(data);
       }
     } catch (e) {
-      console.log('Error get position', e);
-    }
-
-    if (lat && lng) {
-      const {data} = await network.get('api/v1/me/places', {
-        params: {
-          location: {lat, lng},
-          stations: true,
-        },
-      });
-      setStations(data);
+      console.log('Error getting station position', e);
     }
   };
 
