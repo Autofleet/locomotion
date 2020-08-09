3a08ba47f8cba4364de87cbd5bbc2fa94a23cd6c
Ofry Linkovsky <15746116+OfryL@users.noreply.github.com>
2020-08-09 15:56:37 +0300

    fix bug

--- locomotion-sdk/src/pages/ActiveRide/index.js
+++ locomotion-sdk/src/pages/ActiveRide/index.js
@@ -142,7 +142,7 @@ export default ({ navigation, menuSide, mapSettings }) => {
     }
     if (activeRideState) {
       const {data: rideSummary} = await network.get('api/v1/me/rides/ride-summary', {params: {rideId: activeRideState.externalId}});
-      if (rideSummary.state === 'completed') {
+      if (rideSummary && rideSummary.state === 'completed') {
         const pickupTime = rideSummary.stopPoints[0].completedAt;
         const dropoffTime = rideSummary.stopPoints[1].completedAt;
         const distance = rideSummary.stopPoints[1].actualDistance;
