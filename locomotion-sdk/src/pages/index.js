6479a6e1675c0c61f1a6f05bbddf0e8481b83418
Ori Efrati <oritheman132@gmail.com>
2021-06-02 17:00:16 +0300

    old navigator

--- locomotion-sdk/src/pages/index.js
+++ locomotion-sdk/src/pages/index.js
@@ -1,12 +1,10 @@
-import React, { useRef } from 'react';
-import { Dimensions, View, Text } from 'react-native';
-
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { NavigationContainer } from '@react-navigation/native';
-import { createSwitchNavigator } from '@react-navigation/compat';
+import React from 'react';
+import { Dimensions, View } from 'react-native';
+import {
+  createSwitchNavigator, createStackNavigator, createAppContainer, createDrawerNavigator,
+} from 'react-navigation';
 import i18n from '../I18n';
-import NavigationService from '../services/navigation';
+
 import AuthLoadingScreen from './Login/AuthLoadingScreen';
 import Auth from '../services/auth';
 import Login from './Login';
@@ -16,9 +14,8 @@ import RideHistory from './RideHistory';
 import ContactUs from './ContactUs';
 import Onboarding from './Onboarding';
 import Lock from './Lock';
-import Logout from './Logout';
-import { DrawerContentComponent, DrawerLabel, Icon } from '../Components/Drawer';
-import AppSettings from '../services/app-settings';
+import Payments from './Payments';
+import { DrawerContentComponent, DrawerLabel } from '../Components/Drawer';
 
 const PlusIconSource = require('../assets/plus.png');
 const CarIconSource = require('../assets/menuItems/car.png');
@@ -28,174 +25,84 @@ const PplIconSource = require('../assets/menuItems/person.png');
 const HomeIconSource = require('../assets/menuItems/home.png');
 const LogoutIconSource = require('../assets/menuItems/logout.png');
 
-const drawerWidth = Dimensions.get('window').width / 1.25;
+const drawerWidth = Dimensions.get('window').width;
 const activeBackgroundColor = '#ffffff';
 const inactiveBackgroundColor = '#ffffff';
 
-const screensOptions = {
-  Home: {
-    screen: ActiveRide,
-    title: i18n.t('menu.home'),
-    icon: HomeIconSource
-  },
-  RideHistory: {
-    screen: RideHistory,
-    title: i18n.t('menu.trips'),
-    icon: CarIconSource
-  },
-  Account: {
-    screen: Onboarding,
-    title: i18n.t('menu.account'),
-    icon: PplIconSource
-  },
-  ContactUs: {
-    screen: ContactUs,
-    title: i18n.t('menu.support'),
-    icon: HelpIconSource
-  },
-  Logout: {
-    screen: Logout,
-    title: i18n.t('menu.logout'),
-    icon: LogoutIconSource
-  },
-}
 export const MainRouter = (props) => {
-  const navigatorRef = useRef(null);
+  const addPageProps = Page => navigationProps => (<Page {...navigationProps} {...props} />);
 
-  const addPageProps = Page => (navigationProps = {}) => (<Page {...navigationProps} />);
-  const DrawerNavigator = createDrawerNavigator();
+  const AppStack = createDrawerNavigator({
+    Home: {
+      screen: addPageProps(ActiveRide),
+      navigationOptions: {
+        drawerLabel: (<DrawerLabel title={i18n.t('menu.home')} icon={HomeIconSource} />),
+      },
+    },
+    RideHistory: {
+      screen: addPageProps(RideHistory),
+      navigationOptions: {
+        drawerLabel: (<DrawerLabel title={i18n.t('menu.trips')} icon={CarIconSource} />),
+      },
+    },
+     Payment: {
+       screen: addPageProps(Payments),
+       navigationOptions: {
+         drawerLabel: (<DrawerLabel title={i18n.t('menu.paymentsSettings')} icon={CreaditCardIconSource} />),
+       },
+     },
+    Account: {
+      screen: addPageProps(Onboarding),
+      navigationOptions: {
+        drawerLabel: (<DrawerLabel title={i18n.t('menu.account')} icon={PplIconSource} />),
+      },
+    },
+    ContactUs: {
+      screen: addPageProps(ContactUs),
+      navigationOptions: {
+        drawerLabel: (<DrawerLabel title={i18n.t('menu.support')} icon={HelpIconSource} />),
+      },
+    },
 
-  const Drawer = () => (
-    <DrawerNavigator.Navigator
-      initialRouteName="Home"
-      drawerContent={(props) => <DrawerContentComponent
-        {...props}
-        screensOptions={screensOptions}
-        inactiveBackgroundColor="#ffffff"
-        activeBackgroundColor="#ffffff"
-        itemStyle={{
-          borderColor: '#dfdfdf',
-          borderBottomWidth: 1,
-        }}
-        />}
-        drawerStyle={{
-          width: drawerWidth
-        }}
-        detachInactiveScreens={false}
-        drawerType="front"
-        unmountOnBlur={false}
+    Logout: {
+      screen: (({ navigation }) => {
+        Auth.logout(navigation);
+        return (<View />);
+      }),
+      navigationOptions: {
+        drawerLabel: (<DrawerLabel title={i18n.t('menu.logout')} icon={LogoutIconSource} />),
+      },
+    },
+  }, {
+    initialRouteName: 'Home',
+    drawerWidth,
+    contentComponent: (props)=> (<DrawerContentComponent {...props} />),
+    contentOptions: {
+      inactiveBackgroundColor: '#ffffff',
+      activeBackgroundColor: '#ffffff',
+    },
+  });
 
+  const AuthStack = createStackNavigator({ SignIn: addPageProps(Login), Onboarding: addPageProps(Onboarding) }, {
+    headerMode: 'none',
+  });
 
-    >
-      {Object.keys(screensOptions).map(k => {
-        return (
-          <DrawerNavigator.Screen
-          name={k}
-          component={addPageProps(screensOptions[k].screen)}
-          options={{
-           // drawerLabel: () => (<View><Text>xxxxxx</Text><Arrow /></View>),//screensOptions[k].title,
-           drawerLabel: screensOptions[k].title,
-            drawerIcon: () => (<Icon icon={screensOptions[k].icon} />),
-            itemStyle:{backgroundColor: 'red'}
-          }}
-
-          />
-        )
-      })}
-    </DrawerNavigator.Navigator>
-  );
-
-  const AppStack = createNativeStackNavigator();
-  const AuthStackNavigator = () => (
-
-    <AppStack.Navigator
-      screenOptions={{
-        screenOrientation: 'portrait_up',
-        stackAnimation: 'none',
-        headerStyle: {
-          backgroundColor: 'rgba(246, 246, 248, 1)',
-          boxShadow: null,
-          elevation: 0, // remove shadow on Android
-          shadowOpacity: 0, // remove shadow on iOS
-          marginBottom: 0,
-          borderBottomWidth: 1,
-          borderColor: 'rgba(0,0,0,.1)',
-        },
-        headerTitleStyle: {
-          fontFamily: 'Montserrat-Regular',
-          fontWeight: 'normal',
-        },
-        headerBackTitle: i18n.t('navigation.back'),
-      }}
-    >
-      <AppStack.Screen name="SignIn" component={addPageProps(Login)} options={{ headerShown: false }} />
-      <AppStack.Screen name="Onboarding" component={addPageProps(Onboarding)} options={{ headerShown: false }} />
-    </AppStack.Navigator>
-  );
-
-
-  const MainStack = createNativeStackNavigator();
-  const MainStackNavigator = () => (
-
-    <MainStack.Navigator
-      screenOptions={{
-        screenOrientation: 'portrait_up',
-        stackAnimation: 'none',
-        headerStyle: {
-          backgroundColor: 'rgba(246, 246, 248, 1)',
-          boxShadow: null,
-          elevation: 0, // remove shadow on Android
-          shadowOpacity: 0, // remove shadow on iOS
-          marginBottom: 0,
-          borderBottomWidth: 1,
-          borderColor: 'rgba(0,0,0,.1)',
-        },
-        headerTitleStyle: {
-          fontFamily: 'Montserrat-Regular',
-          fontWeight: 'normal',
-        },
-        headerBackTitle: i18n.t('navigation.back'),
-      }}
-    >
-      <MainStack.Screen name="AuthLoading" component={addPageProps(AuthLoadingScreen)} options={{ headerShown: false }} />
-      <MainStack.Screen name="Onboarding" component={addPageProps(Onboarding)} options={{ headerShown: false }} />
-      <MainStack.Screen name="Auth" component={addPageProps(AuthStackNavigator)} options={{ headerShown: false }} />
-      <MainStack.Screen name="Lock" component={addPageProps(Lock)} options={{ headerShown: false }} />
-      <MainStack.Screen name="App" component={Drawer} options={{ headerShown: false }} />
-    </MainStack.Navigator>
-  );
-
-
-
-  const TopLevelNavigator = createSwitchNavigator(
+  const Router = createAppContainer(createSwitchNavigator(
     {
       AuthLoading: addPageProps(AuthLoadingScreen),
-      App: Drawer,
-      Auth: AuthStackNavigator,
+      App: AppStack,
+      Auth: AuthStack,
       Lock,
       Onboarding,
     },
     {
       initialRouteName: 'AuthLoading',
-      navigationOptions: {
-        stackAnimation: 'none',
-      },
-      resetOnBlur: false
+      headerMode: 'none',
     },
-  );
+  ));
 
-  return (
-    <NavigationContainer
-      ref={navigatorRef}
-      onReady={() => {
-        if (navigatorRef) {
-          NavigationService.setTopLevelNavigator(navigatorRef.current);
-        }
-      }}
-    >
-      <TopLevelNavigator />
-    </NavigationContainer>
-  );
+
+  return (<Router screenProps={{ ...props }} />);
 };
 
 
