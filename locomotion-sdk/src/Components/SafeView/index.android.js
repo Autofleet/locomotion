4a4625567255f6327ff3f8bf5ad7d3110c801887
Ori Efrati <oritheman132@gmail.com>
2021-06-15 10:43:11 +0300

    fix android safe area

--- locomotion-sdk/src/Components/SafeView/index.android.js
+++ locomotion-sdk/src/Components/SafeView/index.android.js
@@ -3,10 +3,6 @@ import { Platform, SafeAreaView } from 'react-native';
 import DeviceInfo from 'react-native-device-info';
 
 export default ({ children, style, ...props }) => {
-  if (Platform.OS === 'android' && DeviceInfo.hasNotch()) {
-    SafeAreaView.setStatusBarHeight(35);
-  }
-
   return (
     <SafeAreaView {...props} style={style}>
       {children}
