ad8768bb40a7caf78e721787c3dc2a79da4a8ee5
Ori Efrati <oritheman132@gmail.com>
2022-04-27 23:36:25 +0300

    Update nexmo.js

--- server/src/lib/nexmo.js
+++ server/src/lib/nexmo.js
@@ -8,8 +8,8 @@ class NexmoService {
     this.virtualNumber = process.env.NEXMO_VIRTUAL_NUMBER;
 
     this.nexmo = new Nexmo({
-      apiKey: process.env.NEXMO_API_KEY || 'd4e4f493',
-      apiSecret: process.env.NEXMO_API_SECRET || 'TIURWV0FcLlYCoAr',
+      apiKey: process.env.NEXMO_API_KEY,
+      apiSecret: process.env.NEXMO_API_SECRET,
     });
   }
 
