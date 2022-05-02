020a690cc7e58346d4404019137b39a9ffc988fb
Aaron Lieblich <lieblichaaron@gmail.com>
2022-05-02 16:21:01 +0300

    remove localhost

--- locomotion-sdk/src/services/app-settings.js
+++ locomotion-sdk/src/services/app-settings.js
@@ -18,7 +18,7 @@ const AppSettings = {
     const res = await Storage.get(keyName);
     return res || {};
   },
-  getServerUrl: async () => 'http://localhost:8085/', // const { serverUrl } = await AppSettings.getSettings();
+  getServerUrl: async () => SERVER_HOST, // const { serverUrl } = await AppSettings.getSettings();
   // return serverUrl && serverUrl.length > 0 ? serverUrl : SERVER_HOST;
 
 
