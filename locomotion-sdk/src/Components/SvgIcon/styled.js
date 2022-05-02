cc3a142a1347057df12de666da9ea25f05a12527
Omer Gery <omergery1996@gmail.com>
2022-05-02 13:15:40 +0300

    Revert "Merge pull request #54 from Autofleet/setup-svgs"
    This reverts commit 6f795605350bde41966cf22076884951277e43c6, reversing
    changes made to 1c5a79ad1a68594e3111395f24b946df28d0648f.

--- locomotion-sdk/src/Components/SvgIcon/styled.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-export const IconContainer = styled.View`
-    margin: ${({ margin }) => (margin ? `${margin}` : '0')};
-    vertical-align: middle;
-    width: ${({ width }) => (width ? `${width}` : '16px')};
-    height: ${({ height }) => (height ? `${height}` : '16px')};
-`;
