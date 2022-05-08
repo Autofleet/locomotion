b9f9595b1e1d175a81c308e1cde9f4e5eecbf5cd
Aaron Lieblich <lieblichaaron@gmail.com>
2022-05-08 17:29:42 +0300

    wip

--- examples/client/Locomotion/ios/Locomotion/AppDelegate.mm
+++ examples/client/Locomotion/ios/Locomotion/AppDelegate.mm
@@ -15,6 +15,7 @@
 #import <ReactCommon/RCTTurboModuleManager.h>
 
 #import <react/config/ReactNativeConfig.h>
+#import <GoogleMaps/GoogleMaps.h>
 
 @interface AppDelegate () <RCTCxxBridgeDelegate, RCTTurboModuleManagerDelegate> {
   RCTTurboModuleManager *_turboModuleManager;
@@ -29,6 +30,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [GMSServices provideAPIKey:@"AIzaSyB-c9iBOz8CmNyynP74oH5RIFtdClfFlXw"];
   RCTAppSetupPrepareApp(application);
 
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
