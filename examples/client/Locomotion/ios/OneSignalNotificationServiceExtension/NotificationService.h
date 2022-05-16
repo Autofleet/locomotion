3cc6a5f743facb4e3c8ea99ee54c6d024d4371e7
Aviv Bar Niv <aviv8151@gmail.com>
2022-05-16 20:59:51 +0300

    side

---
+++ examples/client/Locomotion/ios/OneSignalNotificationServiceExtension/NotificationService.h
@@ -0,0 +1,13 @@
+//
+//  NotificationService.h
+//  OneSignalNotificationServiceExtension
+//
+//  Created by Dor Shay on 11/05/2018.
+//  Copyright © 2018 Facebook. All rights reserved.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
