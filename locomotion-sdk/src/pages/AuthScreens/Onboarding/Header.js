23b4ed924278ef2e6233ff70124d44d7273f327e
Aaron Lieblich <lieblichaaron@gmail.com>
2022-05-16 15:12:14 +0300

    wip

--- locomotion-sdk/src/pages/AuthScreens/Onboarding/Header.js
+++ locomotion-sdk/src/pages/AuthScreens/Onboarding/Header.js
@@ -4,9 +4,6 @@ import PageHeader from "../../../Components/PageHeader";
 import backArrow from '../../../assets/arrow-back.png'
 const Header = ({title}) => {
     const navigation = useNavigation()
-    const route = useRoute()
-    console.log(navigation.getParent().getId(), route.name)
-    const canGoBack = navigation.getParent().getId() !== 'authStack' || route.name === 'Phone';
     return (
         <PageHeader 
             title={title} 
