14f44049122557cb2e765f9e287c7030ec6d8a48
Ofry Linkovsky <15746116+OfryL@users.noreply.github.com>
2022-05-18 11:44:01 +0300

    tsc

---
+++ locomotion-sdk/src/pages/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Main from './Main';
+import Auth from './Auth';
+import AuthLoadingScreen from './Login/AuthLoadingScreen';
+import OnboardingContext from '../context/onboarding';
+
+const Stack = createNativeStackNavigator();
+
+const MainRouter = () => (
+  <NavigationContainer>
+    <OnboardingContext.Provider>
+      <Stack.Navigator initialRouteName="AuthLoading" screenOptions={{headerShown: false}} id="authStack">
+        <Stack.Screen name="AuthScreens" component={Auth} options={{gestureEnabled: false}}/>
+        <Stack.Screen name="MainApp" component={Main}/>
+        <Stack.Screen name="AuthLoading" component={AuthLoadingScreen}/>
+      </Stack.Navigator>
+    </OnboardingContext.Provider>
+  </NavigationContainer>
+);
+
+export default MainRouter;
