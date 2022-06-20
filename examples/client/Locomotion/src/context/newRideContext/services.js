30cc93f09150c0bfa2fb1610b33c92703d9c5bfa
Aaron Lieblich <lieblichaaron@gmail.com>
2022-06-20 11:48:39 +0300

    en file tags

--- examples/client/Locomotion/src/context/newRideContext/services.js
+++ examples/client/Locomotion/src/context/newRideContext/services.js
@@ -1,6 +1,8 @@
+import i18n from "../../I18n";
+
 export const TAG_OPTIONS = {
-  FASTEST: 'Fastest',
-  CHEAPEST: 'Cheapest',
+  FASTEST: i18n.t('services.tags.fastest'),
+  CHEAPEST: i18n.t('services.tags.cheapest'),
 };
 
 export const formatEstimationsResult = (service, estimationResult) => {
