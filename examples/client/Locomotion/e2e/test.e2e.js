d2124ed2ffa7ace363b0f79876794d3addf773c8
Omer Gery <omergery1996@gmail.com>
2022-06-21 13:00:37 +0300

    e2e kickoff

---
+++ examples/client/Locomotion/e2e/test.e2e.js
@@ -0,0 +1,10 @@
+describe('Detox Demo', () => {
+    beforeEach(async () => {
+    //   await device.reloadReactNative();
+    console.log('hello');
+    });
+  
+    it('should have header with  title Detox Demo', async () => {
+        console.log('hello');
+    });
+  });
