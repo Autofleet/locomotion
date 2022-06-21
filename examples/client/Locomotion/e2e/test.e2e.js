8235e56e097cc663db38c2ad2958f27abed3a929
Omer Gery <omergery1996@gmail.com>
2022-06-21 17:29:41 +0300

    wip

--- examples/client/Locomotion/e2e/test.e2e.js
+++ examples/client/Locomotion/e2e/test.e2e.js
@@ -1,12 +1,12 @@
 describe('Detox Demo', () => {
     beforeEach(async () => {
-    //   await device.reloadReactNative();
+      await device.reloadReactNative();
     console.log('hello');
     });
   
     it('should have header with  title Detox Demo', async () => {
     console.log('hello');
-    //   await expect(element(by.id('demo-header'))).toBeVisible();
-    //   await expect(element(by.id('demo-header'))).toHaveText('Detox Demo');
+      await expect(element(by.id('demo-header'))).toBeVisible();
+      await expect(element(by.id('demo-header'))).toHaveText('Detox Demo');
     });
   });
