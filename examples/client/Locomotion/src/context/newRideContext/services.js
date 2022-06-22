f5349a9413c539b26c39597db3f9dc032c8c4ff4
Aaron Lieblich <lieblichaaron@gmail.com>
2022-06-22 12:39:31 +0300

    add currency symbol

--- examples/client/Locomotion/src/context/newRideContext/services.js
+++ examples/client/Locomotion/src/context/newRideContext/services.js
@@ -5,17 +5,21 @@ export const TAG_OPTIONS = {
   CHEAPEST: i18n.t('services.tags.cheapest'),
 };
 
-export const formatEstimationsResult = (service, estimationResult) => ({
-  id: service.id,
-  name: service.displayName,
-  eta: (estimationResult || {}).minPickupEta,
-  price: (estimationResult || {}).priceAmount,
-  availableSeats: service.maxPassengers || 4,
-  tag: null,
-  iconUrl: service.icon,
-  description: service.displayDescription,
-  priority: service.priority,
-});
+export const formatEstimationsResult = (service, estimationResult) => {
+  const estimation = estimationResult || {};
+  return {
+    id: service.id,
+    name: service.displayName,
+    eta: estimation.minPickupEta,
+    price: estimation.priceAmount,
+    currency: estimation.priceCurrency,
+    availableSeats: service.maxPassengers || 4,
+    tag: null,
+    iconUrl: service.icon,
+    description: service.displayDescription,
+    priority: service.priority,
+  };
+};
 
 export const formatStopPointsForEstimations = requestStopPoints => requestStopPoints.map(sp => ({
   type: sp.type,
