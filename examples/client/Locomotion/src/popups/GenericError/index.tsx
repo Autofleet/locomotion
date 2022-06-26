47fe029c19f8d6b616cff661adc29de326794c70
Aaron Lieblich <lieblichaaron@gmail.com>
2022-06-26 09:58:21 +0300

    fix props

--- examples/client/Locomotion/src/popups/GenericError/index.tsx
+++ examples/client/Locomotion/src/popups/GenericError/index.tsx
@@ -8,22 +8,42 @@ import {
   ButtonText, CloseButton, Container, TextContainer, TitleContainer,
 } from './styles';
 
-export default ({ isVisible, closePopup }:
-    { isVisible: boolean, closePopup: SetStateAction<any> }) => (
-      <Modal isVisible={isVisible}>
-        <Container>
-          <TitleContainer>
-            <SvgIcon Svg={errorIcon} height={20} width={20} style={{ marginRight: 5 }} />
-            <Title>{i18n.t('popups.genericError.title')}</Title>
-          </TitleContainer>
-          <TextContainer>
-            <SubTitle>{i18n.t('popups.genericError.text')}</SubTitle>
-          </TextContainer>
-          <CloseButton onPress={closePopup}>
-            <ButtonText>
-              {i18n.t('popups.genericError.buttonText')}
-            </ButtonText>
-          </CloseButton>
-        </Container>
-      </Modal>
+interface GenericErrorProps {
+  isVisible: boolean;
+  closePopup: SetStateAction<boolean>;
+  title?: string;
+  text?: string;
+  buttonText?: string
+}
+
+const GenericErrorPopup = ({
+  isVisible,
+  closePopup,
+  title,
+  text,
+  buttonText,
+}: GenericErrorProps) => (
+  <Modal isVisible={isVisible}>
+    <Container>
+      <TitleContainer>
+        <SvgIcon Svg={errorIcon} height={20} width={20} style={{ marginRight: 5 }} />
+        <Title>{title}</Title>
+      </TitleContainer>
+      <TextContainer>
+        <SubTitle>{text}</SubTitle>
+      </TextContainer>
+      <CloseButton onPress={closePopup}>
+        <ButtonText>
+          {buttonText}
+        </ButtonText>
+      </CloseButton>
+    </Container>
+  </Modal>
 );
+
+GenericErrorPopup.defaultProps = {
+  title: i18n.t('popups.genericError.title'),
+  text: i18n.t('popups.genericError.text'),
+  buttonText: i18n.t('popups.genericError.buttonText'),
+};
+export default GenericErrorPopup;
