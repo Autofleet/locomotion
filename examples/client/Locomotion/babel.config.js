937e1847d3fbb31f743eba81979a1be30ec2d740
Omer Gery <omergery1996@gmail.com>
2022-06-27 13:32:29 +0300

    fix build

--- examples/client/Locomotion/babel.config.js
+++ examples/client/Locomotion/babel.config.js
@@ -2,11 +2,10 @@ module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
     'react-native-reanimated/plugin',
-    'jsx-property-alias',
-    {
+    ['jsx-property-alias', {
       properties: {
         testID: 'accessibilityLabel',
       },
-    },
+    }],
   ],
 };
