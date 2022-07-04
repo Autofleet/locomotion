6272ef4788a05bc0ad2d971dc07667ad06d957cf
shirmagen <shir@autofleet.io>
2022-07-04 17:58:22 +0300

    fix delay

--- examples/client/Locomotion/src/pages/Payments/credit-cards.tsx
+++ examples/client/Locomotion/src/pages/Payments/credit-cards.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { View } from 'react-native';
+import { Promise } from 'bluebird';
 import i18n from '../../I18n';
 import {
   DeleteCreditCard,
@@ -17,24 +18,14 @@ export default ({
   onDetach = (id: string) => null,
   loadingState = false,
   onAddClick = undefined,
+  isCashEnabled = false,
 }) => {
   const [loading, setLoading] = useState(false);
-  const [isCashEnabled, setIsCashEnabled] = useState(false);
   const usePayments = PaymentsContext.useContainer();
-
 
   useEffect(() => {
     setLoading(loadingState);
   }, [loading]);
-
-  useEffect(() => {
-    const getIsCashEnabled = async () => {
-      const result = await usePayments.isCashPaymentEnabled();
-      setIsCashEnabled(result.value);
-    };
-
-    getIsCashEnabled();
-  }, [usePayments]);
 
 
   return (
