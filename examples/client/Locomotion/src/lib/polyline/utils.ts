b3fded3990e22b47285a8182669dd0303544b48c
Aaron Lieblich <lieblichaaron@gmail.com>
2022-07-04 14:37:50 +0300

    lint

--- examples/client/Locomotion/src/lib/polyline/utils.ts
+++ examples/client/Locomotion/src/lib/polyline/utils.ts
@@ -1,8 +1,8 @@
-import CheapRuler from 'cheap-ruler';
+import CheapRuler, { Point } from 'cheap-ruler';
 
-export const getSubLineStringAfterLocationFromDecodedPolyline = (pointsFromPolyline, location) => {
+export const getSubLineStringAfterLocationFromDecodedPolyline = (pointsFromPolyline: Point[], location: {latitude: number; longitude: number}) => {
   const ruler = new CheapRuler(location.latitude, 'meters');
-  const origin = [location.latitude, location.longitude];
+  const origin: Point = [location.latitude, location.longitude];
 
   const pointOnLine = ruler.pointOnLine(pointsFromPolyline, origin).point;
   if (pointOnLine[0] === 0.0 && pointOnLine[1] === 0.0) {
