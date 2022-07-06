0f9e714eb3688538bb4d7991c621e5f78ff14a07
shirmagen <shir@autofleet.io>
2022-07-06 17:49:59 +0300

    wip

--- examples/client/Locomotion/src/pages/Payments/index.js
+++ examples/client/Locomotion/src/pages/Payments/index.js
@@ -26,14 +26,11 @@ export default ({ navigation, menuSide }) => {
   const [methodForDelete, setMethodForDelete] = useState(null);
   const [, togglePopup] = getTogglePopupsState();
   const hasPaymentMethods = paymentMethods && paymentMethods.length > 0;
-  const [isCashEnabled, setIsCashEnabled] = useState(false);
   const [showList, setShowList] = useState(hasPaymentMethods);
 
   const loadCustomerData = async () => {
     await usePayments.getOrFetchCustomer();
     setPageLoading(false);
-    const result = await usePayments.isCashPaymentEnabled();
-    setIsCashEnabled(result.value);
   };
 
   useEffect(() => {
@@ -78,7 +75,6 @@ export default ({ navigation, menuSide }) => {
         {/* <Balance customer={usePayments.customer} /> */}
         {showList ? (
           <CreditCardsList
-            isCashEnabled={isCashEnabled}
             paymentMethods={usePayments.paymentMethods}
             onDetach={onRemoveMethod}
             loadingState={loading}
