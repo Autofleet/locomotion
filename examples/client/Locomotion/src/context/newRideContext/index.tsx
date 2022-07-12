ef59fe9b910c3e3ef32551aac34f15c2d9adc417
Aviv Bar Niv <aviv8151@gmail.com>
2022-07-12 12:06:58 +0300

    fix

--- examples/client/Locomotion/src/context/newRideContext/index.tsx
+++ examples/client/Locomotion/src/context/newRideContext/index.tsx
@@ -285,6 +285,9 @@ const RidePageContextProvider = ({ children }: {
       const formattedRide = await formatRide(activeRide);
       setRide(formattedRide);
       changeBsPage(BS_PAGES.ACTIVE_RIDE);
+    } else {
+      cleanRideState();
+      changeBsPage(BS_PAGES.ADDRESS_SELECTOR);
     }
   };
 
@@ -294,24 +297,29 @@ const RidePageContextProvider = ({ children }: {
   }, []);
 
   useInterval(async () => {
-    if (!rideRequestLoading) {
+    if (!rideRequestLoading && user) {
       if (ride?.id) {
-        const rideLoaded = await rideApi.getRide(ride?.id);
-        const formattedRide = await formatRide(rideLoaded);
-        if (ride.state !== rideLoaded.state) {
-          const screenFunction = RIDE_STATES_TO_SCREENS[rideLoaded.state];
-          if (screenFunction) {
-            screenFunction(rideLoaded);
+        try {
+          const rideLoaded = await rideApi.getRide(ride?.id);
+          const formattedRide = await formatRide(rideLoaded);
+          if (ride.state !== rideLoaded.state) {
+            const screenFunction = RIDE_STATES_TO_SCREENS[rideLoaded.state];
+            if (screenFunction) {
+              screenFunction(rideLoaded);
+            }
           }
-        }
-        if (!RIDE_FINAL_STATES.includes(ride?.state || '')) {
-          setRide(formattedRide);
+          if (!RIDE_FINAL_STATES.includes(ride?.state || '')) {
+            setRide(formattedRide);
+          }
+        } catch (e) {
+          cleanRideState();
+          changeBsPage(BS_PAGES.ADDRESS_SELECTOR);
         }
       } else {
         loadActiveRide();
       }
     }
-  }, 3000);
+  }, 4000);
 
 
   useEffect(() => {
