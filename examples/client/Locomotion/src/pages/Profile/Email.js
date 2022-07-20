b24c995772f070a049d31f3f4a1779e1fc4f20ff
Aviv Bar Niv <aviv8151@gmail.com>
2022-07-20 10:55:14 +0300

    special

--- examples/client/Locomotion/src/pages/Profile/Email.js
+++ examples/client/Locomotion/src/pages/Profile/Email.js
@@ -54,7 +54,7 @@ const Email = ({ navigation }) => {
   };
 
   const emailSchema = yup.object().shape({
-    email: yup.string().required().email(),
+    email: yup.string().required().email().matches(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/),
   });
 
   const onChange = async (value) => {
