8d3bd09bb8f8c9397704e50e27bf39b82641b2a3
Aaron Lieblich <lieblichaaron@gmail.com>
2022-07-21 18:38:14 +0300

    test

--- examples/client/Locomotion/src/pages/ActiveRide/index.js
+++ examples/client/Locomotion/src/pages/ActiveRide/index.js
@@ -111,6 +111,8 @@ const RidePage = ({ mapSettings, navigation }) => {
       initSps();
     } else if (serviceEstimations) {
       changeBsPage(BS_PAGES.SERVICE_ESTIMATIONS);
+    } else if (currentBsPage === BS_PAGES.CONFIRM_FUTURE_RIDE) {
+      resetStateToAddressSelector();
     } else {
       // sorry
       setAddressSelectorFocus(selectedInputIndex === 0
