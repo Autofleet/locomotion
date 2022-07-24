ed1476cc08a087a4089df7e5e519482fbb051347
Aaron Lieblich <lieblichaaron@gmail.com>
2022-07-24 18:38:59 +0300

    mend

--- examples/client/Locomotion/src/context/newRideContext/index.tsx
+++ examples/client/Locomotion/src/context/newRideContext/index.tsx
@@ -593,7 +593,7 @@ const RidePageContextProvider = ({ children }: {
     const history = await getLastAddresses();
     setHistoryResults(history);
   };
-  console.log('ride', ride);
+
   const FAILED_TO_CREATE_RIDE_ACTIONS = {
     [RIDE_FAILED_REASONS.BUSY]: () => { changeBsPage(BS_PAGES.NO_AVAILABLE_VEHICLES); },
     [RIDE_FAILED_REASONS.USER_FUTURE_RIDE_INTERVAL_LIMIT_REACHED]: () => {
