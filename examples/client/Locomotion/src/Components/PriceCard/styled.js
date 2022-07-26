1d23261119f1352a534be59a429cfe4002966955
shirmagen <shir@autofleet.io>
2022-07-26 17:58:53 +0300

    wip

--- examples/client/Locomotion/src/Components/PriceCard/styled.js
+++ examples/client/Locomotion/src/Components/PriceCard/styled.js
@@ -1,5 +1,5 @@
-import { FONT_SIZES, FONT_WEIGHTS } from '../../context/theme';
 import styled from 'styled-components';
+import { FONT_SIZES, FONT_WEIGHTS } from '../../context/theme';
 
 export const PriceCardContainer = styled.View`
   flex-direction: row;
@@ -8,12 +8,12 @@ export const PriceCardContainer = styled.View`
   justify-content: space-between;
    `;
 
-   export const PriceNameText = styled.Text`
-    ${FONT_SIZES.LARGE};
+export const PriceNameText = styled.Text`
+${FONT_SIZES.LARGE};
 ${FONT_WEIGHTS.REGULAR};
 opacity: .8;
    `;
 
-   export const PriceText = styled(PriceNameText)`
+export const PriceText = styled(PriceNameText)`
    text-align: right;
    `;
