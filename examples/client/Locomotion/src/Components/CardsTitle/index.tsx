6dc5d3b381f0205c7ca15c948810ee65c9bc7646
Aviv Bar Niv <aviv8151@gmail.com>
2022-07-26 18:49:16 +0300

    side

--- examples/client/Locomotion/src/Components/CardsTitle/index.tsx
+++ examples/client/Locomotion/src/Components/CardsTitle/index.tsx
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 import { FONT_SIZES, FONT_WEIGHTS } from '../../context/theme';
 
 type StyledCardsTitleInterface = {
-  noPaddingLeft: boolean;
+  noPaddingLeft: boolean | undefined;
 }
 
 const StyledCardsTitle = styled(Text)<StyledCardsTitleInterface>`
