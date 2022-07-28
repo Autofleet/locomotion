637afba76a0d70e83d67c57e224fdb64d321413d
Aaron Lieblich <lieblichaaron@gmail.com>
2022-07-28 18:07:16 +0300

    fix

--- examples/client/Locomotion/src/pages/ActiveRide/index.js
+++ examples/client/Locomotion/src/pages/ActiveRide/index.js
@@ -233,7 +233,7 @@ const RidePage = ({ mapSettings, navigation }) => {
       && currentBsPage === BS_PAGES.ADDRESS_SELECTOR) {
       changeBsPage(BS_PAGES.LOCATION_REQUEST);
     }
-    if (!ride.id) {
+    if (locationGranted !== undefined && !ride.id) {
       focusCurrentLocation();
     }
   }, [locationGranted]);
