326c7ba19a2b0ec85d9139afb60d89829cd14143
Aviv Bar Niv <aviv8151@gmail.com>
2022-07-28 11:57:58 +0300

    side

--- examples/client/Locomotion/src/services/one-signal.js
+++ examples/client/Locomotion/src/services/one-signal.js
@@ -5,6 +5,7 @@ import Config from 'react-native-config';
 import network from './network';
 import { updateUser } from '../context/user/api';
 import { StorageService } from '.';
+import Mixpanel from './Mixpanel';
 
 
 class NotificationsService {
@@ -12,7 +13,28 @@ class NotificationsService {
     this.network = network;
   }
 
-  init = (notificationsHandlers) => {
+  updateServer = async (pushToken, userId) => {
+    const clientProfile = await StorageService.get('clientProfile');
+    if (clientProfile.pushUserId !== userId || clientProfile.pushToken !== pushToken) {
+      this.registerOnServer({
+        pushToken,
+        pushUserId: userId,
+      });
+    }
+  };
+
+  checkLatestDeviceState = async () => {
+    const state = await OneSignal.getDeviceState();
+    Mixpanel.setEvent('Notification Service: Check App State', state || undefined);
+    if (state) {
+      const { pushToken, userId } = state;
+      if (pushToken && userId) {
+        await this.updateServer(pushToken, userId);
+      }
+    }
+  };
+
+  init = async (notificationsHandlers) => {
     this.notificationsHandlers = notificationsHandlers;
 
     OneSignal.setAppId(Config.ONESIGNAL_APP_ID);
@@ -20,9 +42,18 @@ class NotificationsService {
     OneSignal.disablePush(false);
 
     if (Platform.OS === 'ios') {
-      OneSignal.promptForPushNotificationsWithUserResponse(() => null);
+      OneSignal.promptForPushNotificationsWithUserResponse((response) => {
+        if (response) {
+          Mixpanel.setEvent('iOS User approved push');
+        }
+        Mixpanel.setEvent('iOS User didn\'t approved push');
+        return null;
+      });
     }
     OneSignal.addSubscriptionObserver(this.subscriptionObserverHandler);
+    OneSignal.addPermissionObserver(this.checkLatestDeviceState);
+
+    await this.checkLatestDeviceState();
   };
 
   getDeviceState = async () => {
@@ -34,13 +65,7 @@ class NotificationsService {
     const { to } = data;
     const { pushToken, userId } = to;
     if (pushToken && userId) {
-      const clientProfile = await StorageService.get('clientProfile');
-      if (clientProfile.pushUserId !== userId || clientProfile.pushToken !== pushToken) {
-        this.registerOnServer({
-          pushToken,
-          pushUserId: userId,
-        });
-      }
+      await this.updateServer(pushToken, userId);
     }
   };
 
