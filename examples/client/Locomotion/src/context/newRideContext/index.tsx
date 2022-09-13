14a6c904d4d52b9c162923233daf17102b59bbe9
Aaron Lieblich <lieblichaaron@gmail.com>
2022-09-13 18:03:52 +0300

    mend

--- examples/client/Locomotion/src/context/newRideContext/index.tsx
+++ examples/client/Locomotion/src/context/newRideContext/index.tsx
@@ -332,7 +332,6 @@ const RidePageContextProvider = ({ children }: {
       Mixpanel.setEvent('Get service estimations');
       const { estimations, services } = await rideApi
         .createServiceEstimations(formattedStopPoints, ride.scheduledTo);
-      console.log(estimations);
       const tags = getEstimationTags(estimations);
       const formattedEstimations = formatEstimations(services, estimations, tags);
       setChosenService(ride.scheduledTo ? formattedEstimations[0]
