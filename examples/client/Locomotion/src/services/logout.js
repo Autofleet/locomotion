3604da76d14617fbc64b4bcd083af3bd6f0784f5
Omer Gery <68545675+OmerGery@users.noreply.github.com>
2022-11-17 12:25:39 +0200

    naming

--- examples/client/Locomotion/src/services/logout.js
+++ examples/client/Locomotion/src/services/logout.js
@@ -1,14 +1,14 @@
 import Auth from './auth';
 import network from './network';
 
-export const logoutUser = async () => {
+export const logoutUserApi = async () => {
   const { data } = await network.post('api/v1/me/logout');
   return data;
 };
 
 export const logout = async (navigation = null) => {
   try {
-    await logoutUser();
+    await logoutUserApi();
     await Auth.logout();
   } catch (e) {
     console.error('error when try to logout', e);
