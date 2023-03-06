a0631f9d971baa1becdf0a3d99b3be9fa470eef9
Aviv Bar Niv <aviv8151@gmail.com>
2023-03-06 15:22:28 +0200

    latest changes

--- examples/client/Locomotion/src/LocomotionRouter.js
+++ examples/client/Locomotion/src/LocomotionRouter.js
@@ -16,30 +16,15 @@ import FutureRidesProvider from './context/futureRides';
 import MessagesProvider from './context/messages';
 import CancellationReasonsProvider from './context/cancellation-reasons';
 import VirtualStationsProvider from './context/virtualStationsContext';
-import GenericErrorPopup from './popups/GenericError';
-import networkInfo from './services/networkInfo';
 
 LogBox.ignoreAllLogs();
 
 export default (props) => {
   const navigatorRef = useRef(null);
-  const [isConnected, setIsConnected] = useState(true);
 
   useEffect(() => {
     crashlytics().log('App mounted.');
     enableScreens(false);
-    let unsubscribeFunction;
-    setTimeout(() => {
-      unsubscribeFunction = networkInfo.addEventListener((listener) => {
-        setIsConnected(listener.isConnected && listener.isInternetReachable);
-      });
-    }, 3000);
-
-    return () => {
-      if (unsubscribeFunction) {
-        unsubscribeFunction();
-      }
-    };
   }, []);
 
   return (
@@ -63,15 +48,6 @@ export default (props) => {
                         <MainRouter {...props} />
                         {props.children}
                         <RidePopups />
-                        <GenericErrorPopup
-                          isVisible={!isConnected}
-                          title={i18n.t('popups.noConnection.title')}
-                          text={i18n.t('popups.noConnection.text')}
-                          buttonText={i18n.t('popups.noConnection.buttonText')}
-                          closePopup={() => {
-                            setIsConnected(true);
-                          }}
-                        />
                       </PortalProvider>
                     </MessagesProvider>
                   </NewRidePageContextProvider>
