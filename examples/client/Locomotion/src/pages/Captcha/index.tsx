c6fc2caa818b5899f20b89f6b431eea01d3f6ada
Omer Gery <68545675+OmerGery@users.noreply.github.com>
2023-05-11 17:41:25 +0300

    test invisible mode

--- examples/client/Locomotion/src/pages/Captcha/index.tsx
+++ examples/client/Locomotion/src/pages/Captcha/index.tsx
@@ -1,31 +1,43 @@
-import React, { useEffect, useRef } from 'react';
-import { View, Button } from 'react-native';
+import React, { useCallback, useEffect, useRef } from 'react';
+import { View, Alert } from 'react-native';
 import Config from 'react-native-config';
 import Recaptcha, { RecaptchaHandles } from 'react-native-recaptcha-that-works';
+import { Text } from 'react-native-svg';
+import { NavButton } from '../Profile/SaveButton/styles';
 
 // ...
 
 const Captcha = () => {
   const recaptcha = useRef<RecaptchaHandles>(null);
 
-  useEffect(() => {
-    if (recaptcha?.current) {
-      recaptcha.current.open();
+  const handleOpenPress = useCallback(() => {
+    console.log('open sesame');
+    if (recaptcha.current) {
+      console.log('opening');
+      recaptcha.current?.open();
     }
-  }, [recaptcha]);
+  }, []);
 
   const onVerify = (token: string) => {
-    console.log('success!', token);
+    Alert.alert('success!', token);
+  };
+  const onLoad = () => {
+    console.log('loaded');
   };
 
   return (Config.CAPTCHA_KEY ? (
     <View>
+      <NavButton onPress={handleOpenPress} />
       <Recaptcha
         ref={recaptcha}
         siteKey={Config.CAPTCHA_KEY}
+        onLoad={onLoad}
         baseUrl="https://www.google.com/recaptcha/api/siteverify"
         onVerify={onVerify}
-        size="normal"
+        size="invisible"
+        enterprise={false}
+        hideBadge={false}
+        footerComponent={<Text>Footer here</Text>}
       />
     </View>
   ) : <></>);
