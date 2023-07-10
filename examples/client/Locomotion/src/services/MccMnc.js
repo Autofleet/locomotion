3259ef122b8085d33b3577b712860bbe7291d06b
roei-bracha <roei@autofleet.io>
2023-07-10 16:10:23 +0300

    Fix linting

--- examples/client/Locomotion/src/services/MccMnc.js
+++ examples/client/Locomotion/src/services/MccMnc.js
@@ -1,7 +1,7 @@
 import MccMncList from 'mcc-mnc-list';
 import CarrierInfo from 'react-native-carrier-info';
 import Config from 'react-native-config';
-import mixpanel from "services/Mixpanel";
+import mixpanel from 'services/Mixpanel';
 
 const IP_LOCATION_API = Config.IP_LOCATION_API || 'https://ipapi.co/json/';
 
@@ -21,7 +21,7 @@ export const getCountryCodeByIP = async () => {
     const data = await response.json();
     return data?.country;
   } catch (error) {
-    mixpanel.trackWithProperties("Unable to get country code by IP", { error })
+    mixpanel.trackWithProperties('Unable to get country code by IP', { error });
     return null;
   }
 };
@@ -45,7 +45,7 @@ export const getInputIsoCode = async () => {
     const IsoByMncMcc = mmcMnc ? getIsoCodeByList(mmcMnc, mobileIso) : null;
     return IsoByMncMcc || mobileIso || await getCountryCodeByIP() || defaultCountryCode;
   } catch (error) {
-    mixpanel.trackWithProperties("Unable to get input iso code from sim", { error })
+    mixpanel.trackWithProperties('Unable to get input iso code from sim', { error });
     return await getCountryCodeByIP() || defaultCountryCode;
   }
 };
