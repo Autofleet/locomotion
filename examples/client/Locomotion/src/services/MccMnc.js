693d9fa1980161b39d634cec7e2d70c3cd89ea58
roei-bracha <roei@autofleet.io>
2023-07-10 15:25:12 +0300

    add a fallback to use IP to get county code

--- examples/client/Locomotion/src/services/MccMnc.js
+++ examples/client/Locomotion/src/services/MccMnc.js
@@ -2,7 +2,7 @@ import MccMncList from 'mcc-mnc-list';
 import CarrierInfo from 'react-native-carrier-info';
 import Config from 'react-native-config';
 
-const defaultCountryCode = Config.DEFAULT_COUNTRY_CODE;
+export const defaultCountryCode = Config.DEFAULT_COUNTRY_CODE;
 const getMccMnc = async () => {
   const mccMnc = await CarrierInfo.mobileNetworkOperator();
   return mccMnc;
@@ -11,6 +11,16 @@ const getMccMnc = async () => {
 const getMobileIsoCode = async () => {
   const isoCode = await CarrierInfo.isoCountryCode();
   return isoCode.toUpperCase();
+};
+export const getCountryCodeByIP = async () => {
+  try {
+    const response = await fetch('https://ipapi.co/json/');
+    const data = await response.json();
+    return data?.country;
+  } catch (error) {
+    console.log('Unable to get country code by IP', error);
+    return null;
+  }
 };
 
 const getIsoCodeByList = (mccMnc, mobileIso) => {
@@ -30,9 +40,9 @@ export const getInputIsoCode = async () => {
     ]);
 
     const IsoByMncMcc = mmcMnc ? getIsoCodeByList(mmcMnc, mobileIso) : null;
-    return IsoByMncMcc || mobileIso || defaultCountryCode;
+    return IsoByMncMcc || mobileIso || await getCountryCodeByIP() || defaultCountryCode;
   } catch (error) {
     console.error('cannot get iso code', error);
-    return defaultCountryCode;
+    return await getCountryCodeByIP() || defaultCountryCode;
   }
 };
