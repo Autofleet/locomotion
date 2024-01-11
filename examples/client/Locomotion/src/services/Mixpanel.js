6691b601fb8f13b8528ab30a3b7189a46cff36b3
Ori Efrati <oritheman132@gmail.com>
2024-01-11 12:17:36 +0200

    filter users events

--- examples/client/Locomotion/src/services/Mixpanel.js
+++ examples/client/Locomotion/src/services/Mixpanel.js
@@ -1,21 +1,50 @@
 /* eslint-disable class-methods-use-this */
 import Config from 'react-native-config';
 import { Mixpanel } from 'mixpanel-react-native';
+import { getUniqueId } from 'react-native-device-info';
+import { Alert } from 'react-native';
 import { getDeviceId } from './device';
 
 export const getElementName = props => props.testID || props.id;
+
+const getRandomNumberByUuid = (uuid) => {
+  try {
+    const decimalValue = parseInt(uuid.substr(0, 8), 16);
+    const mappedNumber = (decimalValue % 100) + 1;
+    return mappedNumber;
+  } catch (err) {
+    console.log('getRandomNumberByUuid error', err);
+    return 0;
+  }
+};
+
+const { MIXPANEL_EVENTS_NUMBER } = Config;
+
+const shouldTrackEvents = () => {
+  if (!MIXPANEL_EVENTS_NUMBER) {
+    return true;
+  }
+  const deviceUuid = getUniqueId();
+  const number = getRandomNumberByUuid(deviceUuid);
+  Alert.alert(deviceUuid, number.toString());
+  return number <= parseInt(MIXPANEL_EVENTS_NUMBER, 10);
+};
 
 class MixpanelService {
   constructor() {
     this.isInit = false;
     this.mixpanel = {};
-    this.init();
+    this.shouldTrackEvents = shouldTrackEvents();
+    if (this.shouldTrackEvents) {
+      this.init();
+    }
   }
 
   init = async () => {
     if (!this.isInit && Config.MIXPANEL_TOKEN) {
       const trackAutomaticEvents = true;
       this.mixpanel = new Mixpanel(Config.MIXPANEL_TOKEN, trackAutomaticEvents);
+
       this.mixpanel.init();
       this.mixpanel.setLoggingEnabled(true);
       this.isInit = true;
@@ -23,9 +52,11 @@ class MixpanelService {
   };
 
   setUser = async (user) => {
+    if (!this.isInit) return;
+
     const uniqueId = (user && user.id) || getDeviceId();
     this.user = user;
-    if (user && user.id) {
+    if (user && user.id && this.isInit) {
       this.mixpanel.optInTracking();
       this.mixpanel.identify(uniqueId);
       this.mixpanel.getPeople().set({
@@ -37,6 +68,7 @@ class MixpanelService {
   };
 
   trackWithProperties = (event, props) => {
+    if (!this.isInit) return;
     if (this.isInit && this.mixpanel) {
       this.mixpanel.track(event,
         {
@@ -64,6 +96,7 @@ class MixpanelService {
   };
 
   trackElementClick = (props, properties = {}) => {
+    if (!this.isInit) return;
     const elmName = getElementName(props);
     const eventName = `${elmName}`;
     if (elmName) {
@@ -77,6 +110,7 @@ class MixpanelService {
   };
 
   registerSuperProperties = (properties) => {
+    if (!this.isInit) return;
     this.mixpanel.registerSuperProperties(properties);
   };
 
