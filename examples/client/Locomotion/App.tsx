9460adb76306d89a9923b8c77ffd7ac5371ad08f
Aaron Lieblich <aaronlieblich@Aarons-MacBook-Pro.local>
2025-09-04 13:17:18 +0300

    fix merge

--- examples/client/Locomotion/App.tsx
+++ examples/client/Locomotion/App.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
+import { StatusBar } from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import LocomotionRouter from './src/LocomotionRouter';
 // import de from './src/I18n/en.json';
 
 const App = () => (
-  <LocomotionRouter
+  <SafeAreaProvider>
+    <StatusBar translucent={false} barStyle="dark-content" />
+    <SafeAreaView style={{ flex: 1 }} edges={['top', 'left', 'right', 'bottom']}>
+
+      <LocomotionRouter
     /*     i18n={{default: 'de', translations: [{lang:'de', translation:de}]}} */
-    menuSide="right"
-    /* mapSettings={{
-      customMapStyle: [{
-        featureType: 'poi',
-        stylers: [{ visibility: 'off' }],
-      }],
-    }} */
-  />
+        menuSide="right"
+      />
+    </SafeAreaView>
+  </SafeAreaProvider>
+
 );
 
 export default App;
