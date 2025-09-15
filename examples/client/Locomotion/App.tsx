08fb8cfd69ae0345b02b4f7fb1c6dc23111fc080
Davidi Shohat <davidishohat7@gmail.com>
2025-09-15 16:37:28 +0300

    Align app.tsx with CI

--- examples/client/Locomotion/App.tsx
+++ examples/client/Locomotion/App.tsx
@@ -1,17 +1,17 @@
-import React from 'react';
 import { StatusBar } from 'react-native';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import LocomotionRouter from './src/LocomotionRouter';
-// import de from './src/I18n/en.json';
+import customLogo from './src/assets/logo.png';
+import customI18n from './src/I18n/en.json';
 
 const App = () => (
   <SafeAreaProvider>
     <StatusBar translucent={false} barStyle="dark-content" />
     <SafeAreaView style={{ flex: 1 }} edges={['top', 'left', 'right', 'bottom']}>
-
       <LocomotionRouter
-    /*     i18n={{default: 'de', translations: [{lang:'de', translation:de}]}} */
+        i18n={{ default: 'en', translations: [{ lang: 'en', translation: customI18n }] }}
         menuSide="right"
+        logo={customLogo}
       />
     </SafeAreaView>
   </SafeAreaProvider>
