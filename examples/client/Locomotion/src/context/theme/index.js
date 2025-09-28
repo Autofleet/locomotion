20be486c828b48d8b84e137f8307e1a024e80ba4
Davidi Shohat <davidishohat7@gmail.com>
2025-09-28 14:42:20 +0300

    Version 4.0.8

--- examples/client/Locomotion/src/context/theme/index.js
+++ examples/client/Locomotion/src/context/theme/index.js
@@ -24,11 +24,14 @@ export const convertHextToRgba = (hex, alpha) => {
   return `${r},${g},${b},${alpha}`;
 };
 
+export const ERROR_COLOR = '#f35657';
+
 export const darkTheme = {
   primaryColor: '#38a7fc',
   secondaryColor: '#08902d',
   primaryButtonTextColor: '#ffffff',
   disabledColor: '#bcbcbc',
+  errorColor: ERROR_COLOR,
 
   textColor: '#ffffff',
   pageBackgroundColor: '#24292E',
@@ -40,6 +43,7 @@ export const lightTheme = {
   secondaryColor: SECONDARY_COLOR || '#08902d',
   primaryButtonTextColor: '#ffffff',
   disabledColor: '#bcbcbc',
+  errorColor: ERROR_COLOR,
 
   /** for dark mode: */
   textColor: '#000000',
@@ -52,7 +56,6 @@ export const THEME_MOD = {
 };
 
 export const GREEN_COLOR = '#2cc36a';
-export const ERROR_COLOR = '#f35657';
 export const LINK_BLUE_COLOR = '#38a7fc';
 export const FONT_SIZES = {
   H1: 'font-size: 22px',
